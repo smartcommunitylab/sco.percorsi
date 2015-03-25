a31dfa6cc2a3ef4f1cafa53ad3858801622417de
oskarnrk <o.zambotti@fbk.eu>
2015-03-25 15:19:48 +0100

    Paths list data rendered better

--- percorsi-mobile/www/js/controllers/paths.js
+++ percorsi-mobile/www/js/controllers/paths.js
@@ -3,14 +3,16 @@ angular.module('roveretoPercorsi.controllers.paths', [])
 .controller('PathsCtrl', function ($scope, $http, $stateParams, listPathsService, singlePathService) {
     $scope.paths = {};
     $scope.noMorePathsAvailable = false;
-    $scope.loadMore = function () {
 
+    $scope.loadMore = function () {
         var length = 0;
+
         if ($scope.paths.data) {
             length = $scope.paths.data.length;
         }
+
         listPathsService.getPathsByCategoryId($stateParams, length).then(function (paths) {
-            //check state for array
+            //check state for array come funziona
             $scope.emptylist = false;
             if ($scope.paths.data) {
                 $scope.paths.data.push.apply($scope.paths.data, paths.data);
@@ -19,14 +21,15 @@ angular.module('roveretoPercorsi.controllers.paths', [])
                         $scope.noMorePathsAvailable = true;
                     }
                 }
-                /*temp */
+
+                /* temp */
                 if (paths.length == 0) {
                     $scope.noMorePathsAvailable = true;
-
-                } /*temp */
+                } /* temp */
             } else {
                 $scope.paths = paths;
             }
+
             if ($scope.paths.data.length == 0) {
                 $scope.emptylist = true;
             } else {
@@ -36,7 +39,16 @@ angular.module('roveretoPercorsi.controllers.paths', [])
             $scope.$broadcast('scroll.infiniteScrollComplete');
         });
     }
+
     $scope.choosePath = function (path) {
         singlePathService.choosePath(path);
     }
-})
+
+    $scope.m2km = function (m) {
+        return Math.round((m / 1000) * 10) / 10;
+    }
+
+    $scope.min2time = function (min) {
+        return Math.floor(min / 60) + ':' + min % 60;
+    }
+});
