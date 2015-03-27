56e4257c51f7d3d515477e700655943ff26f3296
Matteo Chini <matteo.chini@gmail.com>
2015-03-27 17:46:00 +0100

    poi details modified

--- percorsi-mobile/www/js/directives.js
+++ percorsi-mobile/www/js/directives.js
@@ -60,3 +60,23 @@ angular.module('roveretoPercorsi.directives', [])
         }
     }
 });
+//    .directive('preventDrag', function ($ionicGesture, $ionicSlideBoxDelegate) {
+//        return {
+//            restrict: 'A',
+//
+//            link: function (scope, elem, attrs, e) {
+//                var reportEvent = function (e) {
+//
+//                    if (e.target.tagName.toLowerCase() == 'div') {
+//                        $ionicSlideBoxDelegate.enableSlide(false);
+//                    } else {
+//                        $ionicSlideBoxDelegate.enableSlide(true);
+//                    }
+//                };
+//
+//
+//                $ionicGesture.on('drag', reportEvent, elem);
+//            }
+//        };
+//    })
+//;
