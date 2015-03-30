b5ca0a319593a5f55e8dc50eccd93ecf9738719c
oskarnrk <o.zambotti@fbk.eu>
2015-03-30 11:40:05 +0200

    Itinerary POIs now are clikable

--- percorsi-mobile/www/js/controllers/pathdetailinfo.js
+++ percorsi-mobile/www/js/controllers/pathdetailinfo.js
@@ -1,9 +1,14 @@
 angular.module('roveretoPercorsi.controllers.pathdetailinfo', [])
 
-.controller('PathDetailInfoCtrl', function ($scope, $http, singlePathService) {
+.controller('PathDetailInfoCtrl', function ($scope, $http, singlePathService, singlePoiService) {
     $scope.path = singlePathService.getSelectedPath();
 
     $scope.badabum = function () {
         window.alert('FAB!');
-    }
+    };
+
+    $scope.showPoi = function (poiIndex) {
+        singlePoiService.setSelectedPoi($scope.path.pois[poiIndex]);
+        window.location.assign('#/app/poidetail');
+    };
 });
