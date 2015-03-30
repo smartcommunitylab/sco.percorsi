de4ed64d2a44c991694aedeedd98c85c40683b1c
oskarnrk <o.zambotti@fbk.eu>
2015-03-30 17:49:50 +0200

    Standardizing gallerymodal

--- percorsi-mobile/www/js/controllers/pathdetailinfo.js
+++ percorsi-mobile/www/js/controllers/pathdetailinfo.js
@@ -1,14 +1,15 @@
 angular.module('roveretoPercorsi.controllers.pathdetailinfo', [])
 
 .controller('PathDetailInfoCtrl', function ($scope, $http, $ionicSlideBoxDelegate, $ionicModal, singlePathService, singlePoiService) {
-    $scope.path = singlePathService.getSelectedPath();
+    $scope.item = singlePathService.getSelectedPath();
     singlePoiService.setIndexPoi(null);
+
     $scope.badabum = function () {
         window.alert('FAB!');
     };
 
     $scope.showPoi = function (poiIndex) {
-        singlePoiService.setSelectedPoi($scope.path.pois[poiIndex]);
+        singlePoiService.setSelectedPoi($scope.item.pois[poiIndex]);
         singlePoiService.setIndexPoi(poiIndex);
         window.location.assign('#/app/poidetail');
     };
